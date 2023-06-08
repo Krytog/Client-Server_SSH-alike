0f631bebbc000ea27471d0d49dca6cd9101181fb
Daniil Baranov <danya2003.db@gmail.com>
2023-06-08 03:15:02 +0300

    Add headers with definitions

---
+++ Queris/queris_types.h
@@ -0,0 +1,12 @@
+#define SPAWN 1
+#define LIST 2
+#define ATTACH 3
+#define KILL 4
+#define PEEK 5
+
+#define TYPE_SPAWN_STRING "spawn"
+#define TYPE_LIST_STRING "list"
+#define TYPE_ATTACH_STRING "attach"
+#define TYPE_KILL_STRING "kill"
+#define TYPE_PEEK_STRING "peek"
+#define TYPE_FAILED -1
---
+++ Queris/signals.h
@@ -0,0 +1,2 @@
+#define KEY_EOF 1
+#define KEY_SIGKILL 2
